3aab38cd4b81fe7d217fd17b616bcff0f8fc7152
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2024-02-29 15:50:51 -0300

    integra Iternacionalizacion(i18n)-v0.1

---
+++ src/i18n/index.ts
@@ -0,0 +1,14 @@
+import spanish from './es.json'
+import catalan from './ca.json'
+
+const LANGUAGES = {
+  SPANISH: 'es',
+  CATALAN: 'ca'
+}
+
+export const getI18N = ({ currentLocale = 'es' } : { currentLocale: string | undefined }) => {
+  if (currentLocale === LANGUAGES.SPANISH) return spanish
+  if (currentLocale === LANGUAGES.CATALAN) return catalan
+
+  return spanish
+}
